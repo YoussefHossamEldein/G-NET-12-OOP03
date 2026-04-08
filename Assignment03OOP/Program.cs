71fb4a43164ae14e669a4de142998da45084a33a
youssef hossam eldein <youssefhossameldeinn@gmail.com>
2026-04-08 16:26:35 +0200

    Questons 1 & 2

--- Assignment03OOP/Program.cs
+++ Assignment03OOP/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            #region Question01
+            //a) Has-A-composition
+            //b) Dependency
+            //c) Inheritance (IS - A)
+            //d) HAS-A-Aggregation
+            //e) Dependency
+            #endregion
+
+            #region Question02
+
+            /* a) yes because the derived class inherits it but it will be also accessible within this 
+              derived class only no object or intances from these classes can use it */
+            /* b) private protected is accessible within the same class but protected internal within 
+             * the same assembly */
+            /* C) the sealed class can not be inherited , the sealed method can not be overriden */
+            /* D) Yes , i can not just make an object from static classes */
+
+            #endregion
         }
     }
 }
